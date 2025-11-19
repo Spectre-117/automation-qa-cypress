e91ad604558358ebddcb7e6ce27e30a7d3a5ed88
Serhii <lyalyuksn@gmail.com>
2025-11-20 01:36:04 +0200

    cypress queries are implemented

--- cypress/e2e/auth/login.spec.js
+++ cypress/e2e/auth/login.spec.js
@@ -1,6 +1,40 @@
+import { faker } from '@faker-js/faker';
+
 describe('Authentification - Login', () => {
 
-    it('Should be able to sign up', () => {
+    const userData = {
+        "userFirstName": faker.person.firstName(),
+        "userLastName": faker.person.lastName(),
+        "userPassword" : `Password*${faker.number.int({min: 10, max: 1000})}`,
+        "userEmail" : faker.internet.email()
+    }
+    beforeEach( ()=> {
+
         cy.visit('/');
+        cy.get('.btn-primary').click();
+        cy.get('.modal-content').within(() => {
+            //const userPassword = `Password*${faker.number.int({min:10, max:1000})}`;
+            cy.get('#signupName').type(userData.userFirstName)
+            cy.get('#signupLastName').type(userData.userLastName)
+            cy.get('#signupEmail').type(userData.userEmail)
+            cy.get('#signupPassword').type(userData.userPassword)
+            cy.get('#signupRepeatPassword').type(userData.userPassword)
+            cy.get('.btn-primary').click();
+        })
+        cy.get('.sidebar_btn').contains(' Log out ').click();
+    })
+
+    it('Should be able to sign in', () => {
+        cy.get('.header_right').contains('Sign In').click();
+        cy.get('.modal-content').within(() => {
+            cy.get('#signinEmail').type(userData.userEmail)
+            cy.get('#signinPassword').type(userData.userPassword)
+            cy.get('.btn-primary').click();
+        })
+        cy.location().its('pathname').should("eq", "/panel/garage")
+        cy.get('.panel-page_heading').within(() => {
+            cy.get('.btn-primary').contains('Add car').should("be.visible");
+            cy.get('.btn-primary').contains('Add car').should("be.enabled");
+        });
     })
 })
